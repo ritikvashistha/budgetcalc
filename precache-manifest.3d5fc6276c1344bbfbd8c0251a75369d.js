11eaf12cba36f5466ace6d34531a8370ced96976
ritikvashistha <vashistharitik9@gmail.com>
2019-09-11 07:17:40 +0530

    Updates

---
+++ precache-manifest.3d5fc6276c1344bbfbd8c0251a75369d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8062852636119206036903952d75213",
+    "url": "/budgetcalc/index.html"
+  },
+  {
+    "revision": "dea0f5ff0336e27143d1",
+    "url": "/budgetcalc/static/css/main.ad2c1192.chunk.css"
+  },
+  {
+    "revision": "7d0c6c9a393d9b1f9931",
+    "url": "/budgetcalc/static/js/2.68872a73.chunk.js"
+  },
+  {
+    "revision": "dea0f5ff0336e27143d1",
+    "url": "/budgetcalc/static/js/main.e67901d2.chunk.js"
+  },
+  {
+    "revision": "6d3cc66730e59d56894d",
+    "url": "/budgetcalc/static/js/runtime~main.9eb60afa.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/budgetcalculatorapp/precache-manifest.449d302469c988234279771f7759b490.js"
+  "/budgetcalc/precache-manifest.3d5fc6276c1344bbfbd8c0251a75369d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/budgetcalculatorapp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/budgetcalc/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
